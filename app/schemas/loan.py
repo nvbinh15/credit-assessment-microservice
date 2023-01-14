96adbbebd2d941e94cfb3a4bca6339d2d9715e33
nvbinh15 <binh.pdc01@gmail.com>
2023-01-14 23:23:55 +0800

    Update loan and user schemas

--- app/schemas/loan.py
+++ app/schemas/loan.py
@@ -1,9 +1,24 @@
 from typing import Optional 
 from pydantic import BaseModel
+from enum import IntEnum 
 
+
+class LoanStatus(IntEnum):
+    pending = 0
+    accepted = 1
+    rejected = -1 
+
+
+class LoanPrediction(IntEnum):
+    accepted = 1
+    rejected = -1 
+        
 
 class LoanBase(BaseModel):
     amount: int 
+    term: int # by month
+    prediction: bool 
+    status: LoanStatus = LoanStatus.pending
 
 
 class Loan(LoanBase):
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
+from enum import Enum, IntEnum
 
 
 class UserBase(BaseModel):
@@ -15,3 +16,38 @@ class User(UserBase):
     
     class Config:
         orm_mode = True
+
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+
+
+class CreditType(IntEnum):
+    poor = 0
+    standard = 1
+    good = 2
+
+
+class Education(str, Enum):
+    pass 
+
+
+class PropertyArea(str, Enum):
+    pass 
+
+
+class UserProfile(User):
+    age: Optional[int]
+    gender: Gender = Gender.male
+    is_married: bool = True  
+    dependents: int = 0
+    education: Education 
+    is_self_employed: bool = False 
+    income: int 
+    property_area: PropertyArea
+    coapplicant_income: int = 0
+    credit: Optional[CreditType]
+
+    class Config:
+        orm_mode = True 
