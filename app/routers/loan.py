1a4d6fde8e170b094076eb661a0a98004f818546
nvbinh15 <binh.pdc01@gmail.com>
2023-01-16 14:21:59 +0800

    Implement loan endpoints

--- app/routers/loan.py
+++ app/routers/loan.py
@@ -23,7 +23,10 @@ def get_all_loans(db: Session = Depends(get_db), current_user: User = Depends(ge
 
 @router.post('', status_code=201, response_model=Loan)
 def create_loan(request: LoanBase, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    new_loan = models.Loan(amount=request.amount, user_id=current_user.id)
+    new_loan = models.Loan(
+        description=request.description, amount=request.amount, term=request.term, 
+        coapplicant_income=request.coapplicant_income, status=request.status, user_id=current_user.id
+    )
     db.add(new_loan)
     db.commit()
     db.refresh(new_loan)
@@ -31,15 +34,31 @@ def create_loan(request: LoanBase, db: Session = Depends(get_db), current_user: 
 
 
 @router.get('/{id}')
-def get_loan(id: int):
-    pass 
+def get_loan(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    loan = db.query(models.Loan) \
+        .filter(models.Loan.user_id == current_user.id) \
+        .filter(models.Loan.id == id).first()
+    if not loan:
+        raise HTTPException(status_code=404, detail="Loan not found")
+    return loan
 
 
 @router.put('/{id}', status_code=201, response_model=Loan)
-def update_loan(id: int):
-    pass 
+def update_loan(id: int, request: LoanBase, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    loan_db = db.query(models.Loan).filter(models.Loan.user_id == current_user.id).filter(models.Loan.id == id)
+    if not loan_db.first():
+        raise HTTPException(status_code=404, detail='Loan not found')
+    loan_db.update(request.dict(exclude_unset=True))
+    db.commit()
+    db.refresh(loan_db.first())
+    return loan_db.first()
 
 
 @router.delete('/{id}')
-def delete_loan(id: int):
-    pass
+def delete_loan(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    loan_db = db.query(models.Loan).filter(models.Loan.user_id == current_user.id).filter(models.Loan.id == id)
+    if not loan_db.first():
+        raise HTTPException(status_code=404, detail='Loan not found')
+    loan_db.delete(synchronize_session=False)
+    db.commit()
+    return {'details': 'deleted'}
